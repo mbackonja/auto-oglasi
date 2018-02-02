06d1bfcee90cc38cf85a424c5c90969c0365b6d0
Milan Bačkonja <milan.backonja@hotmail.com>
2018-02-02 17:38:53 +0100

    Add car - Responsibility and images preview

--- app/client/static/partials/add-car/add-car.controller.js
+++ app/client/static/partials/add-car/add-car.controller.js
@@ -1,14 +1,36 @@
 app.controller('AddCarController', ($scope, $http, $location) => {
   $scope.makes = []
   $scope.models = []
+  $scope.step = 1
 
   $scope.data = {
     make: '',
-    model: ''
+    model: '',
+    year: '',
+    mileage: '',
+    price: '',
+    condition: '',
+    displacement: '',
+    fuel_type: '',
+    kw: '',
+    hp: '',
+    description: '',
+    address: '',
+    city: '',
+    phone: '',
+    images: null
   }
 
   $http.get('/api/makes-and-models').then((response) => {
     $scope.makes = response.data.makes
     $scope.models = response.data.models
   })
+
+  $scope.goToStep2 = () => {
+    $scope.step = 2
+  }
+
+  $scope.create = () => {
+    console.log($scope.data)
+  }
 })
